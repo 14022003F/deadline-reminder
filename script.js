f6bf69e64f164a5f36d5164af7dcb3819182fa50
fatma ezzahra <fatmaezzahra52@gmail.com>
2026-02-13 15:47:22 +0100

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,42 @@
+let deadlines = JSON.parse(localStorage.getItem("deadlines")) || [];
+
+function save(){
+  localStorage.setItem("deadlines", JSON.stringify(deadlines));
+}
+
+function addDeadline(){
+  const title = document.getElementById("title").value;
+  const date = document.getElementById("date").value;
+
+  if(!title || !date) return alert("Fill all fields");
+
+  deadlines.push({title, date});
+  save();
+  render();
+}
+
+function render(){
+  const list = document.getElementById("list");
+  list.innerHTML="";
+
+  deadlines.forEach(d=>{
+    const diff = new Date(d.date) - new Date();
+    const hours = Math.floor(diff/1000/60/60);
+    const days = Math.floor(hours/24);
+
+    const div = document.createElement("div");
+    div.className="card";
+
+    if(days < 1) div.classList.add("warning");
+
+    div.innerHTML = `
+      <strong>${d.title}</strong><br>
+      ${days} days ${hours%24} hrs left
+    `;
+
+    list.appendChild(div);
+  });
+}
+
+setInterval(render,1000);
+render();
